99c5c85e82fa347e65749a3a2f283ff1de0d4642
Andrew <andrew@comm-unitysolutions.com>
2025-10-30 21:30:04 -0600

    Fix admin redirect to allow config.yml access and remove deprecated Netlify Identity

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,38 +1,10 @@
 'use client';
 
 import Script from 'next/script';
-import { useEffect } from 'react';
-
-declare global {
-  interface Window {
-    netlifyIdentity?: {
-      on: (event: string, callback: (user: any) => void) => void;
-      open: () => void;
-      close: () => void;
-    };
-  }
-}
 
 export default function AdminPage() {
-  useEffect(() => {
-    // Initialize Netlify Identity widget
-    if (typeof window !== 'undefined' && window.netlifyIdentity) {
-      window.netlifyIdentity.on('init', (user: any) => {
-        if (!user) {
-          window.netlifyIdentity?.on('login', () => {
-            document.location.href = '/admin/';
-          });
-        }
-      });
-    }
-  }, []);
-
   return (
     <>
-      <Script 
-        src="https://identity.netlify.com/v1/netlify-identity-widget.js"
-        strategy="afterInteractive"
-      />
       <Script 
         src="https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js"
         strategy="afterInteractive"
